4965b562930eb79e2697c24580d6ca7326dd0aae
Sambudi Sugya Putra <sambudisp@gmail.com>
2020-01-18 09:31:05 +0700

    gitIgnore update | Background Thread

---
+++ app/src/main/java/com/sambudisp/made/backgroundThread/MainBgThreadActivity.kt
@@ -0,0 +1,13 @@
+package com.sambudisp.made.backgroundThread
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sambudisp.made.R
+
+class MainBgThreadActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_bg_thread)
+    }
+}
